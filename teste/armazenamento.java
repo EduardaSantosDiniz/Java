e5fefddccf64c536777058d22ba1faa7a8972b83
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2023-10-08 15:05:52 -0300

    atuzalizações

--- teste/armazenamento.java
+++ teste/armazenamento.java
@@ -1,5 +1,86 @@
 package Java.teste;
 
 public class armazenamento {
-    
+    import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Livro {
+    private String titulo;
+    private String autor;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [Título: " + titulo + ", Autor: " + autor + "]";
+    }
+}
+
+class Biblioteca {
+    private List<Livro> acervo;
+
+    public Biblioteca() {
+        acervo = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        acervo.add(livro);
+        System.out.println("Livro adicionado com sucesso!");
+    }
+
+    public void listarLivros() {
+        System.out.println("Lista de Livros na Biblioteca:");
+        for (Livro livro : acervo) {
+            System.out.println(livro);
+        }
+    }
+}
+
+public class SistemaBiblioteca {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Adicionar Livro");
+            System.out.println("2. Listar Livros");
+            System.out.println("3. Sair");
+            int escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    scanner.nextLine(); // Limpar o buffer do teclado
+                    System.out.print("Título do Livro: ");
+                    String titulo = scanner.nextLine();
+                    System.out.print("Autor do Livro: ");
+                    String autor = scanner.nextLine();
+                    Livro livro = new Livro(titulo, autor);
+                    biblioteca.adicionarLivro(livro);
+                    break;
+                case 2:
+                    biblioteca.listarLivros();
+                    break;
+                case 3:
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+}
+
 }
