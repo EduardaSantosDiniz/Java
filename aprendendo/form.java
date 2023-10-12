adf44079189c41a7b56ecb0525d6ed7bda87226f
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2023-10-12 17:55:39 -0300

    aprendendo

--- aprendendo/form.java
+++ aprendendo/form.java
@@ -1,8 +1,29 @@
 package Java.aprendendo;
+import java.util.Scanner;
 
 public class Form{
   public static void main(String[]args){
-    
+
+    Scanner escaneador = new Scanner(System.in);
+
+    System.out.println("Nome: ");
+    String nome = escaneador.nextLine();
+
+    System.out.println("Idade: ")
+    Int idade = escaneador.nextInt();
+
+    System.out.println("Email: ")
+    String email = escaneador.next.Line();
+
+    escaneador.close();
+
+
+    System.out.println("Nome "+nome);
+    System.out.println("Idade "+idade);
+     System.out.println("Email "+email);
+
+
+
   }
 
 }
