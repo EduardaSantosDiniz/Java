3902a66810c7cbbd47d51335a3229fd7531d2bf2
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2023-10-15 19:20:21 -0300

    vendo sem comentarios

--- Projetos/ReservaFacil.java
+++ Projetos/ReservaFacil.java
@@ -1,51 +1,49 @@
-import java.util.Scanner; // Importa a classe Scanner do pacote java.util.
+import java.util.Scanner;
 
-public class ReservaFacil { // Declara a classe ReservaDeQuarto.
-    public static void main(String[] args) { // Inicia o método principal.
+public class ReservaFacil {
+    public static void main(String[] args) {
         // Inicialização dos quartos
-        boolean[] quartosDisponiveis = new boolean[5]; // Cria um array de boolean com 5 elementos para representar os quartos.
-
-        // Preenche o array com o valor "true" para todos os quartos.
+        boolean[] quartosDisponiveis = new boolean[5];
         for (int i = 0; i < 5; i++) {
             quartosDisponiveis[i] = true;
         }
 
-        Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para entrada de dados.
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Bem-vindo à reserva de quartos!"); // Exibe uma mensagem de boas-vindas.
-        while (true) { // Inicia um loop infinito.
+        System.out.println("Bem-vindo à reserva de quartos!");
+        while (true) {
             // Mostrar quartos disponíveis
-            System.out.println("\nQuartos Disponíveis:"); // Exibe a lista de quartos disponíveis.
+            System.out.println("\nQuartos Disponíveis:");
             for (int i = 0; i < 5; i++) {
-                if (quartosDisponiveis[i]) { // Verifica se o quarto está disponível.
-                    System.out.println("Quarto " + (i + 1)); // Exibe o número do quarto.
+                if (quartosDisponiveis[i]) {
+                    System.out.println("Quarto " + (i + 1));
                 }
             }
 
             // Escolher um quarto
-            System.out.print("\nEscolha o quarto (1-5) ou digite 0 para sair: "); // Solicita a escolha do quarto.
-            int escolha = scanner.nextInt(); // Lê a escolha do usuário.
+            System.out.print("\nEscolha o quarto (1-5) ou digite 0 para sair: ");
+            int escolha = scanner.nextInt();
 
-            if (escolha == 0) { // Verifica se o usuário escolheu sair.
-                System.out.println("Obrigado por usar nosso serviço de reserva de quartos. Adeus!"); // Exibe uma mensagem de despedida.
-                break; // Sai do loop.
-            } else if (escolha >= 1 && escolha <= 5) { // Verifica se a escolha é um número de quarto válido.
-                if (quartosDisponiveis[escolha - 1]) { // Verifica se o quarto escolhido está disponível.
-                    scanner.nextLine(); // Limpa a entrada de nova linha.
-                    System.out.print("Digite seu nome: "); // Solicita o nome do usuário.
-                    String nome = scanner.nextLine(); // Lê o nome do usuário.
+            if (escolha == 0) {
+                System.out.println("Obrigado por usar nosso serviço de reserva de quartos. Adeus!");
+                break;
+            } else if (escolha >= 1 && escolha <= 5) {
+                if (quartosDisponiveis[escolha - 1]) {
+                    scanner.nextLine(); // Limpar a entrada de nova linha
+                    System.out.print("Digite seu nome: ");
+                    String nome = scanner.nextLine();
 
-                    quartosDisponiveis[escolha - 1] = false; // Marca o quarto como indisponível.
+                    quartosDisponiveis[escolha - 1] = false;
 
-                    System.out.println("\nQuarto " + escolha + " reservado para " + nome + "."); // Exibe mensagem de reserva.
+                    System.out.println("\nQuarto " + escolha + " reservado para " + nome + ".");
                 } else {
-                    System.out.println("Quarto " + escolha + " já está ocupado. Escolha outro quarto."); // Exibe mensagem de quarto ocupado.
+                    System.out.println("Quarto " + escolha + " já está ocupado. Escolha outro quarto.");
                 }
             } else {
-                System.out.println("Escolha inválida. Por favor, escolha um número de quarto válido."); // Exibe mensagem de escolha inválida.
+                System.out.println("Escolha inválida. Por favor, escolha um número de quarto válido.");
             }
         }
 
-        scanner.close(); // Fecha o objeto Scanner.
+        scanner.close();
     }
 }
