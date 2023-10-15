18fcf1dd15f19bd00db861a100f99ae9e556d064
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2023-10-15 19:04:40 -0300

    mudando

--- aprendendo/form.java
+++ aprendendo/form.java
@@ -10,8 +10,8 @@ public class Form{
     String nome = escaneador.nextLine();
 
     System.out.println("Idade: ");
-    int idade = escaneador.nextInt();
-    escaneador.nextLine();
+    String idade = escaneador.nextLine();
+    
 
     System.out.println("Email: ");
     String email = escaneador.nextLine();
