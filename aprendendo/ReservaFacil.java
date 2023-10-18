4ca84c17f4e15fe31e6f3b1f37317dbcc352b85c
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2023-10-18 16:02:58 -0300

    refazendo

--- aprendendo/ReservaFacil.java
+++ aprendendo/ReservaFacil.java
@@ -1,8 +1,26 @@
 package Java.aprendendo;
+import java.util.Scanner;
 
 
 public class ReservaFacil {
     public static void main(String []args){
-        
+        boolean [] quartosDisponiveis = new boolean[5];
+
+
+        for (int i = 0; i <5; i++){
+            quartosDisponiveis[i]= true;
+        }
+        Scanner escanear = new Scanner (System.in);
+
+        System.out.println("Bem vindo ao nosso sistema de reserva de quartos");
+        while (true){
+            System.out.println("\nQuartos Disponiveis: ");
+            for (int i = 0; i < 5; i++){
+                if(quartosDisponiveis [i]){
+                    System.out.println("Qurto "+ (i + 1));
+                }
+            }
+            
+        }
  }
 }
